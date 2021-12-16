15afdc6fc6c3dd695ce9e85e10f842d2592131ac
Artur <info@aigroupusa.com>
2021-12-15 21:34:48 -0500

    commit tree b62a6842680816027c63ef000ba24bef251ce7e5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 let addToy = false;
 
+const toyCollection = document.querySelector("#toy-collection")
+const toysUrl = 'http://localhost:3000/toys'
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +15,39 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  loadsToys();
+
 });
+
+function loadsToys() {
+  fetch(toysUrl)
+  .then(resp => resp.json())
+  .then(results => {
+    results.forEach(toy => addToys(toy))
+  })
+}
+
+function addToys(toy) {
+  const divCard = document.createElement("div");
+  divCard.classList.add("card");
+  toyCollection.appendChild(divCard);
+  
+  const h2 = document.createElement("h2");
+  h2.innerHTML = toy.name
+  divCard.appendChild(h2);
+
+  const img = document.createElement("img");
+  img.src = toy.image;
+  divCard.appendChild(img);
+
+  const p = document.createElement("p");
+  p.innerHTML = toy.likes + "Likes"
+  divCard.appendChild(p);
+
+  const button = document.createElement("button");
+  button.classList.add("like-btn");
+  button.innerHTML = "Like"
+  divCard.appendChild(button);
+  
+}
